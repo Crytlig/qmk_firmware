aa7e7c0e7b3fc58ccc898ade807988eaf58d2e18
Claes <crytlig@hotmail.com>
2022-06-04 12:21:19 +0200

    Added support for unicode. compilation too large thou..

--- keyboards/idobao/id75/keymaps/crytlig75/keymap.c
+++ keyboards/idobao/id75/keymaps/crytlig75/keymap.c
@@ -1,28 +1,8 @@
-/* Copyright 2018 MechMerlin
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
-};
 
 #define S_NO        RSFT(KC_NO)
 #define CS_TRNS     LCTL(LSFT(KC_TRNS))
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -37,8 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_ortho_5x15(
         _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         _______, _______, KC_UP,    _______, _______, _______, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______, KC_BSLS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______, _______, KC_INS,
-        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, UC(0x00E6), _______, _______, KC_INS,
+        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, UC(0x30C4), UC(0x40B), _______, _______, _______,
         _______, _______, _______,  _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -50,26 +30,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, CS_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QMKBEST:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
-      } else {
-        // when keycode QMKBEST is released
-      }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
-  }
-  return true;
-}
--- keyboards/idobao/id75/keymaps/crytlig75/rules.mk
+++ keyboards/idobao/id75/keymaps/crytlig75/rules.mk
@@ -6,11 +6,12 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes        # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+BOOTMAGIC_ENABLE  = yes        # Enable Bootmagic Lite
+MOUSEKEY_ENABLE   = yes        # Mouse keys
+CONSOLE_ENABLE    = yes        # Console for debug
+COMMAND_ENABLE    = yes        # Commands for debug and configuration
+# UNICODEMAP_ENABLE = yes       # Enables UNICODE MAP
+UNICODE_ENABLE = yes
 # NKRO_ENABLE = no            # Enable N-Key Rollover
 # BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 # RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
