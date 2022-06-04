b37f8d37ba582c5393b16b42e10baba4a4758f2c
Claes <crytlig@hotmail.com>
2022-06-04 14:39:48 +0200

    Added æøå to M(2)

--- keyboards/idobao/id75/keymaps/crytlig75/keymap.c
+++ keyboards/idobao/id75/keymaps/crytlig75/keymap.c
@@ -8,14 +8,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_ortho_5x15(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  KC_GRV,  KC_DEL,  KC_PGUP,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC, KC_PGDN,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC, _______,
         TG(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,  KC_ENT,  KC_RSFT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SCLN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, S_NO,    KC_HOME, KC_END,
         KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, MO(2),   KC_RCTL, TG(1),   MO(1),   KC_PSCR
     ),
 
     [1] = LAYOUT_ortho_5x15(
-        _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_PGDN,
         _______, _______, KC_UP,    _______, _______, _______, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, KC_INS,
         _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -23,10 +23,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT_ortho_5x15(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, CS_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,
+        _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______,    UC(0x00E5), _______, _______, _______,
+        _______, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, UC(0x00E6), UC(0x00F8), _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,
+        _______, _______, _______, CS_TRNS, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______
     ),
 };
+
+
+
+    // [3] = LAYOUT_ortho_5x15(
+    //     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_EQL,     KC_GRV,      KC_DEL,  KC_PGUP,
+    //     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,       UC(0x00E5),  KC_BSPC, KC_BSPC,
+    //     TG(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,   UC(0x00E6), UC(0x00F8),  KC_ENT,  KC_ENT,
+    //     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SCLN, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_MINS,    KC_RSFT,     KC_HOME, KC_END,
+    //     KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  KC_QUOT, KC_RALT, KC_RGUI, MO(2),  KC_RCTL,    TG(0),       MO(1),   KC_PSCR
+    // ),
