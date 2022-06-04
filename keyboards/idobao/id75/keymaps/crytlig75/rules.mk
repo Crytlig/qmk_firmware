c33f0898a8edebc4b3803f8fff280c92dff5f366
Claes <crytlig@hotmail.com>
2022-06-04 13:53:20 +0200

    Added comments and cleaned up.

--- keyboards/idobao/id75/keymaps/crytlig75/rules.mk
+++ keyboards/idobao/id75/keymaps/crytlig75/rules.mk
@@ -7,9 +7,9 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE  = yes   # Enable Bootmagic Lite
-CONSOLE_ENABLE 	  = no   # Console for debug
-COMMAND_ENABLE 	  = no   # Commands for debug and configuration
+CONSOLE_ENABLE 	  = no    # Console for debug
+COMMAND_ENABLE 	  = no    # Commands for debug and configuration
 RGB_MATRIX_ENABLE = yes   # RGB Matrix
-# UNICODEMAP_ENABLE = yes       # Enables UNICODE MAP
-UNICODE_ENABLE 	  = yes
-LTO_ENABLE		  = yes
+UNICODEMAP_ENABLE = no    # Enables UNICODE MAP. Map is self maintained in keymap.c
+UNICODE_ENABLE 	  = yes   # Simple UNICODE enable. Requires WinCompose on Windows
+LTO_ENABLE		  = yes   # Reduce firmware binary size
